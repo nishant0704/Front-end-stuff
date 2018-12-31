d494a4f6df7a6d6856b7c817e49cad68021331a7
Nishant Rao <nishanthrao29@gmail.com>
2018-12-31 16:05:32 +0530

    css and final changes

--- Calculator/calc.js
+++ Calculator/calc.js
@@ -1,6 +1,27 @@
+function insert(num){
+  if (document.display.view.value == 'OFF') {
+    document.display.view.value = '';
+  } else if (document.display.view.value == 'ON') {
+    document.display.view.value = '';
+  }
+  document.display.view.value = document.display.view.value+num;
+}
+
+function equal(res){
+  let exp = document.display.view.value
+  if(exp){
+    document.display.view.value = eval(exp);
+  }
+}
+
 function off(){
   document.display.view.value = 'OFF';
   alert('press ON to continue');
+  offBtn = document.querySelector('.calBtn');
+}
+
+function on(){
+  document.display.view.value = 'ON';
 }
 
 
