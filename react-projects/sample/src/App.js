e8e689ab4e95aba36bc6c5840b2b04c1a3ddb578
Nishant Rao <nishanthrao29@gmail.com>
2019-02-02 12:35:21 +0530

    initial

---
+++ react-projects/sample/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Person from './Person/Person';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello There</h1>
+        <Person/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ react-projects/sample/src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const person = () => {
+  return <p>Welcome To You </p>
+};
+
+export default person;
