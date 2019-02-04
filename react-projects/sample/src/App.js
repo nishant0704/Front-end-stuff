bfdd229a1e5ba0f22ff5c02e5f0ddf77cd2a98c2
Nishant Rao <nishanthrao29@gmail.com>
2019-02-04 12:47:59 +0530

    state

--- react-projects/sample/src/App.js
+++ react-projects/sample/src/App.js
@@ -3,13 +3,20 @@ import Person from './Person/Person';
 import './App.css';
 
 class App extends Component {
+  state = {
+    persons : [
+      {name:"Nishant",age:26},
+      {name:"Shashank",age:20},
+      {name:"Drago",age:18}
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>Hello There</h1>
-        <Person name="Nishant" age="26">I m an Engineer</Person>
-        <Person name="Shashank" age="20"/>
-        <Person name="Drago" age="18"/>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>I m an Engineer</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
   }
