55cf7a1ce89f01ad15ab79c5ffb65470091e1374
Nishant Rao <nishanthrao29@gmail.com>
2019-02-04 15:19:55 +0530

    setState and minor style changes

--- react-projects/sample/src/App.js
+++ react-projects/sample/src/App.js
@@ -10,10 +10,28 @@ class App extends Component {
       {name:"Drago",age:18}
     ]
   }
+  changeName = () => {
+    this.setState({
+      persons : [
+        {name:"Nishant Rao",age:26},
+        {name:"Shashank Rao",age:20},
+        {name:"Drago T",age:18}
+      ]
+    });
+  }
   render() {
+    const style={
+      backgroundColor :'blue',
+      border:'1px solid black',
+      padding:'10px',
+      color:'white',
+      borderRadius:'5px',
+      cursor:'pointer'
+    }
     return (
       <div className="App">
         <h1>Hello There</h1>
+        <button style={style} onClick={this.changeName}>Change Name!</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>I m an Engineer</Person>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
